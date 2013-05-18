f53656d4ea3385494827802566d148c389770077
usuario <usuario@10.22.146.5>
2013-05-18 11:11:46 +0100

    All Test passed

--- src/palindromos/Palindromos.java
+++ src/palindromos/Palindromos.java
@@ -1,12 +1,9 @@
 package palindromos;
 
-
 public class Palindromos {
 
-    
-    
     public static void main(String[] args) {
-        int palindromeCandidate= Integer.parseInt(args[1]);
+        int palindromeCandidate = Integer.parseInt(args[1]);
         System.out.println(new PalindromeChecker().isPalindrome(palindromeCandidate));
     }
 }
--- test/palindromos/PalindromosTest.java
+++ test/palindromos/PalindromosTest.java
@@ -5,11 +5,9 @@
 package palindromos;
 
 import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  *
